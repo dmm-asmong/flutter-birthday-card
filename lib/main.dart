40bcbe274e447f291c6868b5c08a83486869a329
Stranger Yang <ccamdol@gmail.com>
2024-04-03 22:18:02 +0900

    과제 1 생일축하카드

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'HappyBirthdayCard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,49 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Happy BirthDay',
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
-      home: const HappyBirthDayCard(),
-    );
-  }
-}
-
-class HappyBirthDayCard extends StatelessWidget {
-  const HappyBirthDayCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Happy Birthday Card'),
-      ),
-      body: Stack(
-        children: [
-          SizedBox.expand(
-            child: Image.network(
-              'https://cdn.pixabay.com/photo/2014/12/16/21/59/gifts-570821_1280.jpg',
-              fit: BoxFit.cover,
-            ),
-          ),
-          const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Happy BirthDay Sam!',
-                  style: TextStyle(fontSize: 50, color: Colors.white),
-                ),
-                Text(
-                  'From Emma',
-                  style: TextStyle(fontSize: 30, color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+    return const MaterialApp(
+      home: HappyBirthDayCard(),
     );
   }
 }
